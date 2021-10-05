7fa1c8bfca71ecb8e8d265675e1fa7ddb37643b2
VeNoM <kumarsks619@gmail.com>
2021-10-06 01:08:52 +0530

    Client - React lazy/suspense for pages ADDED

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,15 @@
 import React, { useEffect } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 import Footer from './components/Footer'
 import Header from './components/Header'
 import CallToAction from './components/CallToAction'
-import About from './components/About'
-import Contact from './components/Contact'
-import Expertise from './components/Expertise'
-import NotFound from './components/NotFound'
-import Home from './components/Home'
-import Projects from './components/Projects'
 import Alerts from './utils/Comp/Alerts'
 import ScrollToTop from './utils/Comp/ScrollToTop'
 import Cursor from './utils/Comp/Cursor'
+import Routes from './Routes'
 import './App.css'
 import './responsive.css'
 
@@ -30,14 +25,7 @@ const App = () => {
                 <Alerts />
                 <ScrollToTop />
                 <Route component={Header} />
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/projects" component={Projects} />
-                    <Route exact path="/expertise" component={Expertise} />
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/contact" component={Contact} />
-                    <Route component={NotFound} />
-                </Switch>
+                <Routes />
                 <Route component={CallToAction} />
                 <Route component={Footer} />
             </Router>
