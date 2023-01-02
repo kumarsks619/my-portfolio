c5088faf4c9d083025b0c6ea5b058fb6eb459067
Shubham Kumar Singh <kumarsks619@gmail.com>
2023-01-02 15:48:41 +0530

    Updated the server deployed URL of render.com

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,7 +1,7 @@
 // export const NODE_ENV = "DEV"
 export const NODE_ENV = 'PROD'
 //export const SERVER_URL = 'http://localhost:5000'
-export const SERVER_URL = 'https://portfolio-venom.herokuapp.com'
+export const SERVER_URL = 'https://my-portfolio-hdfd.onrender.com'
 export const CLIENT_URL = 'https://www.shubhamm.in'
 export const SOCIAL = {
     LINKEDIN: 'https://www.linkedin.com/in/kumarsks619',
